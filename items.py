9df365706e3b67f1a1a3b80b319f9616bd8db6ed
sheweararmani <44094908+sheweararmani@users.noreply.github.com>
2018-10-22 11:46:03 +0100

    SWitems

---
+++ items.py
@@ -0,0 +1,81 @@
+item_lightsaber = {
+    "id": "lighsaber",
+
+    "name": "Lightsaber",
+
+    "description":
+    "________________"
+
+    
+}
+
+item_pc = {
+    "id": "powerconverter",
+
+    "name": "Power Converter",
+
+    "description":
+    "________________",
+
+    
+}
+
+item_landspeeder = {
+    "id": "landspeeder",
+
+    "name": "Landspeeder",
+
+    "description":
+    "________________",
+
+    
+}
+
+item_r2d2 = {
+    "id": "r2d2",
+
+    "name": "R2D2",
+
+    "description": "________________",
+
+    
+
+}
+
+item_handgun = {
+    "id": "handgun",
+    
+    "name": "Handgun",
+
+    "description": "________________",
+
+    
+}
+
+item_chewie = {
+    "id": "chewie",
+    
+    "name": "Chewbacca",
+
+    "description": "________________",
+
+    
+}
+
+item_leia = {
+	"id": "leia",
+
+	"name": "Princess Leia"
+
+	"description": "________________",
+}
+
+items = {
+    "lighsaber": item_lightsaber,
+    "powerconverter": item_pc,
+    "landspeeder": item_landspeeder,
+    "r2d2": item_r2d2,
+    "handgun": item_handgun,
+    "chewie": item_chewie,
+    "leia": item_leia
+}
